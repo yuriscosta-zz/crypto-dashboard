f0c07aec494e92a5766028e2dcf129acbc8321bf
Yuri Costa <yuri.sales@protonmail.com>
2019-01-28 16:52:41 -0300

    Add styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './index.css';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled, { css } from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello
+        <WelcomeMessage />
       </div>
     );
   }
